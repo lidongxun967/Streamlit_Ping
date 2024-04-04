067a299c7ada691b7ceed1bf620b8d729b7b2ca4
Debug967 <89326292+lidongxun967@users.noreply.github.com>
2024-04-04 21:54:56 +0800

    Create St_main.py

---
+++ St_main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import subprocess
+
+def execute_command(command):
+    try:
+        # 执行bash命令，并捕获输出
+        result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        # 返回标准输出
+        return result.stdout
+    except subprocess.CalledProcessError as e:
+        # 如果命令执行失败，则打印错误信息
+        return None
+
+ain = st.input_text("in com")
+
+ao = execute_command(ain)
+
+ao
